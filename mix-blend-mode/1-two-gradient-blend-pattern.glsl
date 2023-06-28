09fdfd145e959c3ebed7e6ced60adb9616f3c06a
alphardex <2582347430@qq.com>
2023-06-28 12:41:25 +0800

    update

--- mix-blend-mode/1-two-gradient-blend-pattern.glsl
+++ mix-blend-mode/1-two-gradient-blend-pattern.glsl
@@ -20,7 +20,6 @@ vec3 blendMultiply(vec3 base,vec3 blend){
 }
 
 vec3 getGrad(vec2 uv){
-    uv=fract(uv*4.);
     vec3 col1=vec3(.969,.639,.482);
     vec3 col2=vec3(1.,.871,.659);
     vec3 col3=vec3(.816,.894,.690);
@@ -29,11 +28,11 @@ vec3 getGrad(vec2 uv){
     vec3 col6=vec3(0.,.522,.784);
     vec3 c=col1;
     float ratio=1./6.;
-    c=mix(c,col2,1.-step(ratio*5.,uv.y));
-    c=mix(c,col3,1.-step(ratio*4.,uv.y));
-    c=mix(c,col4,1.-step(ratio*3.,uv.y));
-    c=mix(c,col5,1.-step(ratio*2.,uv.y));
-    c=mix(c,col6,1.-step(ratio*1.,uv.y));
+    c=mix(c,col2,step(ratio*1.,uv.y));
+    c=mix(c,col3,step(ratio*2.,uv.y));
+    c=mix(c,col4,step(ratio*3.,uv.y));
+    c=mix(c,col5,step(ratio*4.,uv.y));
+    c=mix(c,col6,step(ratio*5.,uv.y));
     return c;
 }
 
@@ -44,14 +43,18 @@ void mainImage(out vec4 fragColor,in vec2 fragCoord){
     p1-=.5;
     p1=rotate(p1,-PI/4.);
     p1+=.5;
+    p1=fract(p1*4.);
     vec3 col1=getGrad(p1);
     
     vec2 p2=uv;
     p2-=.5;
     p2=rotate(p2,PI/4.);
     p2+=.5;
+    p2=fract(p2*4.);
     vec3 col2=getGrad(p2);
     
+    // vec3 c=col1;
+    // vec3 c=col2;
     vec3 c=blendMultiply(col1,col2);
     
     fragColor=vec4(c,1.);
